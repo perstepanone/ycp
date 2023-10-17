64caa0a2caf77d4b9282c5d8db87aa640d3de65f
perstepanone <138379622+perstepanone@users.noreply.github.com>
2023-10-17 21:56:02 +0700

    Add main function
    Signed-off-by: perstepanone <138379622+perstepanone@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 import os
-from sys import version_info, exit
-
+import sys
+import locale
+from services.app import App
+import click
 
 # Information
 __license__ = 'GPL3'
 __version__ = '0.0.0'
 __release__ = False
+
+VERSION_MSG = [
+    'ycp version: {0}'.format(__version__),
+    'Python version: {0}'.format(' '.join(line.strip() for line in sys.version.splitlines())),
+    'Locale: {0}'.format('.'.join(str(s) for s in locale.getlocale())),
+]
 
 
 def version_helper():
@@ -33,7 +41,7 @@ def version_helper():
 # Constants
 YCPDIR = os.path.dirname(__file__)
 VERSION = version_helper()
-PY3 = version_info[0] >= 3
+PY3 = sys.version_info[0] >= 3
 
 # These variables are ignored if the corresponding
 # XDG environment variable is non-empty and absolute
@@ -41,10 +49,42 @@ CACHEDIR = os.path.expanduser('~/.cache/ycp')
 CONFDIR = os.path.expanduser('~/.config/ycp')
 DATADIR = os.path.expanduser('~/.local/share/ycp')
 
+args = None
+
 
 def main() -> None:
-    pass
+    try:
+        locale.setlocale(locale.LC_ALL, '')
+    except locale.Error:
+        print("Warning: Unable to set locale.  Expect encoding problems.")
+
+    level = 'YCP_LEVEL'
+    if level in os.environ and os.environ[level].isdigit():
+        os.environ[level] = str(int(os.environ[level]) + 1)
+    else:
+        os.environ[level] = '1'
+
+    if 'SHELL' not in os.environ:
+        os.environ['SHELL'] = 'sh'
+
+    app = App()
+    # TODO: Add opportunity to save config
+    # TODO: Add some arguments in cli
+
+    try:
+        if not sys.stdin.isatty():
+            sys.stderr.write("Error: Must run ranger from terminal\n")
+            raise SystemExit(1)
+        elif app.username == 'root':
+            pass
+        elif not args.debug:
+            pass
+        app.initialize()
+        app.ui.initialize()
+    except Exception:
+        pass
 
 
 if __name__ == "__main__":
-    exit(main())
+    main()
+    exit(sys.main())
